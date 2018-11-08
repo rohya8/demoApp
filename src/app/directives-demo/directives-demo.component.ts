a38753ef5dd5dcc8c55cd34a252ab24297f31d3c
Rohit Wadke <rohit.m.wadke@gmail.com>
2018-11-08 22:29:08 +0530

    ngFor trackby changes

--- src/app/directives-demo/directives-demo.component.ts
+++ src/app/directives-demo/directives-demo.component.ts
@@ -9,26 +9,38 @@ export class DirectivesDemoComponent implements OnInit {
 
   // numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-  numbersList = [
-    { id: 123, name: 'ABCD' },
-    { id: 23, name: 'LMN' },
-    { id: 123, name: 'EFG' },
-    { id: 23, name: 'PQR' },
-    { id: 123, name: 'PQRST' },
-    { id: 23, name: 'WXYZ' }
-  ];
+  numbersList;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  loadCourses() {
+    this.numbersList = [
+      { id: 123, name: 'ABCD' },
+      { id: 23, name: 'LMN' },
+      { id: 123, name: 'EFG' },
+      { id: 23, name: 'PQR' },
+      { id: 123, name: 'PQRST' },
+      { id: 23, name: 'WXYZ' }
+    ];
+  }
+
   onAdd() {
-    this.numbersList.push({ id: this.numbersList.length, name: this.numbersList.length + 'JKL' });
+    if (!this.numbersList) {
+      this.numbersList = [{ id: 0, name: 'JKL' }];
+    }  else {
+      this.numbersList.push({ id: this.numbersList.length, name: this.numbersList.length + 'JKL' });
+    }
   }
 
   onRemove(item) {
     this.numbersList.splice(this.numbersList.indexOf(item), 1);
+  }
+
+  trackNumberList(index, item) {
+    return item ? item.id : undefined;
   }
 
 }
